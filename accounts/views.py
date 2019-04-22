d30767aa4f0705150d4fb430def92ae514565969
yooseungju <seung338989@gmail.com>
2019-04-22 08:36:59 +0000

    12_make social login

--- accounts/views.py
+++ accounts/views.py
@@ -88,6 +88,7 @@ def delete(request):
     
     
 def profile_update(request):
+    profile = Profile.objects.get_or_create(user=request.user)
     if request.method == 'POST':
         profile_form = ProfileForm(request.POST, instance=request.user.profile)
         if profile_form.is_valid:
--- insta/settings.py
+++ insta/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'imagekit',
     'bootstrap4',
     'posts.apps.PostsConfig',
@@ -41,7 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth.socialaccount.providers.kakao',
 ]
+
+SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -70,6 +78,15 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+ 
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+   
+)
 
 WSGI_APPLICATION = 'insta.wsgi.application'
 
@@ -125,3 +142,5 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = "accounts.User"
+LOGIN_REDIRECT_URL = 'posts:list'
+
